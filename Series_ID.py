28aa612293a811ea94dc342087fd2f87d8e0637a
Addison Ji <addison.ji92@gmail.com>
2019-03-08 09:11:31 -0500

    Create Series_ID.py

---
+++ Series_ID.py
@@ -0,0 +1,59 @@
+# List of all Series ID used in Project
+
+#Aggregate Combinations
+    #All employees, thousands, total nonfarm, seasonally adjusted
+total_nonfarm = "CES0000000001"
+
+#Construction
+    #All employees, thousands, construction, seasonally adjusted
+construction = "CES2000000001"
+
+#Education and Health Services
+    #All employees, thousands, education and health services, seasonally adjusted
+edu_health = "CES6500000001"
+
+#Farming Logging Mining and Natural Resources
+    #All employees, thousands, mining and logging, seasonally adjusted
+mining_logging = "CES1000000001"
+    ##All employees, thousands, support activities for mining, seasonally adjusted
+    #m_l_support = "CES1021300001"
+    
+#Financial Activities Including Real Estate
+    #All employees, thousands, financial activities, seasonally adjusted
+fin_act="CES5500000001"
+    ##All employees, thousands, real estate, seasonally adjusted
+    #real_estate="CES5553100001"
+
+#Government
+    #All employees, thousands, government, seasonally adjusted
+gov="CES9000000001"
+    
+#Information
+    #All employees, thousands, information, seasonally adjusted
+info="CES5000000001"
+    
+#Leisure and Hospitality
+    #All employees, thousands, leisure and hospitality, seasonally adjusted
+leisure_hospi="CES7000000001"
+    
+#Manufacturing
+    #All employees, thousands, manufacturing, seasonally adjusted
+manu="CES3000000001"
+
+#Other Services
+    #All employees, thousands, all other personal services, including photofinishing, seasonally adjusted
+other_ser="CES8081299001"
+    
+#Professional and Business Services
+    #All employees, thousands, professional and business services, seasonally adjusted
+pro_bus="CES6000000001"
+    
+#Public Administration
+    #All employees, thousands, local government general administration, seasonally adjusted
+loc_gov="CES9093292001"
+    ##All employees, thousands, state government general administration, seasonally adjusted
+    ##state_gov="CES9092292001"
+    
+#Trade Transportation and Utilities
+    #All employees, thousands, trade, transportation, and utilities, seasonally adjusted
+tran_util="CES4000000001"
